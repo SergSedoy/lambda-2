6384523f66c82feecb281cd1396e4a84cf4f5a8d
SergSedoy <78666743+SergSedoy@users.noreply.github.com>
2021-03-02 12:41:32 +0300

    испр. замечания

--- homework lambda 1_2/Main.java
+++ homework lambda 1_2/Main.java
@@ -2,8 +2,8 @@ package javaCore.homework1_2;
 
 public class Main {
     public static void main(String[] args) {
-        Worker.OnTaskDoneListener listener = System.out::println;
-        Worker.OnTaskErrorListener listener2 = System.out::println;
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener listener2 = System.out::println;
 
         Worker worker = new Worker(listener);
         worker.start();
